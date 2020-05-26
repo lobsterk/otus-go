6cc83232bd86627746dadb1877aa0243f1a4c363
Kopylov Danil <lobsterk@yandex.ru>
2020-05-26 23:55:13 +0300

    HW1 is completed

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw01_hello_now
+module github.com/lobsterk/otus-go/hw01_hello_now
 
 go 1.14
 
@@ -6,5 +6,5 @@ require (
 	bou.ke/monkey v1.0.2
 	github.com/beevik/ntp v0.2.0
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/tools v0.0.0-20200526202639-5c6ccfdb40a2 // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const (
+	layout  = "2006-01-02 15:04:05 +0000 UTC"
+	ntpHost = "0.beevik-ntp.pool.ntp.org"
+)
+
 func main() {
-	// Place your code here
+	currentTime := time.Now().Format(layout)
+	fmt.Printf("current time: %s\n", currentTime)
+
+	ntpTime, err := ntp.Time(ntpHost)
+	if err != nil {
+		log.Fatalf("ntp time error: %s \n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("exact time: %s\n", ntpTime.Format(layout))
 }
