0a90f94761afa524b3ebeb65dfd4de186976c493
Kopylov Danil <lobsterk@yandex.ru>
2020-06-01 22:27:52 +0300

    change Format on Round. Add .String()

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -3,25 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 
 	"github.com/beevik/ntp"
 )
 
 const (
-	layout  = "2006-01-02 15:04:05 +0000 UTC"
 	ntpHost = "0.beevik-ntp.pool.ntp.org"
 )
 
 func main() {
-	currentTime := time.Now().Format(layout)
-	fmt.Printf("current time: %s\n", currentTime)
+	currentTime := time.Now().Round(0)
+	fmt.Printf("current time: %s\n", currentTime.String())
 
 	ntpTime, err := ntp.Time(ntpHost)
 	if err != nil {
 		log.Fatalf("ntp time error: %s \n", err)
-		os.Exit(1)
 	}
-	fmt.Printf("exact time: %s\n", ntpTime.Format(layout))
+	exactTime := ntpTime.Round(0)
+	fmt.Printf("exact time: %s", exactTime.String())
 }
