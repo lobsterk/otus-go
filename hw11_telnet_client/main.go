db28680797b3076f65533a8f9dd0587e22bb5705
Kopylov Danil <lobsterk@yandex.ru>
2020-09-30 21:48:55 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,78 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+const argsLen = 4
+
+var timeout time.Duration
+
+func init() {
+	flag.DurationVar(&timeout, "timeout", 0, "connection timeout")
+}
+
 func main() {
-	// Place your code here
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	log.Print("Start")
+	flag.Parse()
+
+	log.Print("Args check")
+	args := os.Args
+	if len(args) != argsLen {
+		log.Fatal("few arguments: should be " + strconv.Itoa(argsLen))
+	}
+
+	log.Print("Args check was ok")
+	host := args[argsLen-2]
+	port := args[argsLen-1]
+
+	client := NewTelnetClient(
+		net.JoinHostPort(host, port),
+		timeout,
+		os.Stdin,
+		os.Stdout,
+	)
+	errorsCh := make(chan error)
+	signlsCh := make(chan os.Signal, 1)
+
+	signal.Notify(signlsCh, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		errorsCh <- client.Send()
+	}()
+	go func() {
+		errorsCh <- client.Receive()
+	}()
+
+	select {
+	case <-signlsCh:
+		signal.Stop(signlsCh)
+
+		return
+	case err = <-errorsCh:
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(os.Stderr, "...EOF\n")
+
+		return
+	}
 }
