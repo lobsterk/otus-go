a9be9710436c0860ec5d8a73e875ca113f161828
Kopylov Danil <lobsterk@yandex.ru>
2020-09-30 21:18:06 +0300

    HW7 is completed

--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,5 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/lobsterk/otus-go/hw07_file_copying
 
 go 1.14
+
+require github.com/cheggaaa/pb/v3 v3.0.5
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 var (
@@ -18,5 +19,14 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here
+
+	if from == "" || to == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	err := Copy(from, to, offset, limit)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
