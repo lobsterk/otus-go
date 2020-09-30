7f21db19541b6fdd077ae84fa01ca310fd9687a2
Kopylov Danil <lobsterk@yandex.ru>
2020-09-30 21:22:56 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,56 @@
 package main
 
+import (
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	var result Environment
+
+	absPath, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	files, err := ioutil.ReadDir(absPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filename := file.Name()
+
+		b, err := ioutil.ReadFile(filepath.Join(dir, filename))
+		if err != nil {
+			return nil, err
+		}
+		if len(b) == 0 {
+			delete(result, filename)
+			continue
+		}
+		b = bytes.ReplaceAll(bytes.Split(b, []byte("\n"))[0], []byte("\x00"), []byte("\n"))
+
+		value := strings.TrimRight(string(b), " \t\n")
+		if strings.ContainsRune(value, '=') {
+			continue
+		}
+
+		if result == nil {
+			result = make(Environment)
+		}
+		result[filename] = value
+	}
+
+	return result, err
 }
